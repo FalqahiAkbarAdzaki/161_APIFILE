43813b2eb779f450030ff9d27412a971c7f696f9
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-11-25 14:47:13 +0700

    revisi dan pelengkapan

--- config/config.js
+++ config/config.js
@@ -6,3 +6,30 @@ const development = {
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  database_test: process.env.DB_DATABASE_TEST,
+  database_production: process.env.DB_DATABASE_PRODUCTION
+}
+const production = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  database_test: process.env.DB_DATABASE_TEST,
+  database_production: process.env.DB_DATABASE_PRODUCTION
+}
+
+const test = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_TEST,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  database_test: process.env.DB_DATABASE_TEST,
+  database_production: process.env.DB_DATABASE_PRODUCTION
+}
+
+module.exports = { development, production, test };
